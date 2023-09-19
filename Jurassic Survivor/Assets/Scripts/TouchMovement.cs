a96c413bab718de8adebeba061a0139c3fc1192b
Raidanin <116862789+Raidanin@users.noreply.github.com>
2023-09-19 15:06:07 +0900

    기본 세팅

---
+++ Jurassic Survivor/Assets/Scripts/TouchMovement.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TouchMovement : MonoBehaviour
+{
+
+}
